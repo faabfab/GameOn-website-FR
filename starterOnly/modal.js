05460e3786d709ed606ea4e40b5698a6ad966b89
faabfab <faab.fab@free.fr>
2023-05-21 11:42:48 +0200

    close button

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,29 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// TODO: Implémenter entrées du formulaire
+// TODO: Ajouter validation ou messages d'erreur
+// TODO: Ajouter confirmation quand envoi réussi
+// TODO: Tests manuels
+
+//  DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector("#btn-close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeBtn.addEventListener("click", closehModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// launch modal form
+function closehModal() {
+  modalbg.style.display = "none";
+}
