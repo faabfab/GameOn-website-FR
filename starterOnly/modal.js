4c5e7152e47da5259d60b4435ad33d97984e816f
faabfab <faab.fab@free.fr>
2023-05-25 19:52:33 +0200

    validation in progress 1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,12 +18,21 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector("#btn-close");
 
+const myForm = document.getElementById('myForm');
+
+const cgvCheckbox = document.getElementById('checkbox1')
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // close modal event
 closeBtn.addEventListener("click", closehModal);
 
+// validation form event
+myForm.addEventListener('submit', validate);
+
+// cgv checkbox event
+cgvCheckbox.addEventListener('click', cgvCheck)
 
 // launch modal form
 function launchModal() {
@@ -33,6 +42,23 @@ function launchModal() {
 // close modal form
 function closehModal() {
   modalbg.style.display = "none";
+}
+
+// cgv check event
+function cgvCheck() {
+  /**
+   * au click on on enleve ou pas le check
+   */
+  if (cgvCheckbox.checked === true) {
+    //cgvCheckbox.checked = false;
+    console.log('coché');
+    return 'coché'
+  }
+  else{
+    console.log('décoché');
+    //cgvCheckbox.checked = true;
+    return 'décoché'
+  }
 }
 
  /**
@@ -50,60 +76,89 @@ function closehModal() {
 
 
  function validate(){
-
-  // Récupération des variables
-  let first = document.forms["reserve"]["first"];
-  let last = document.forms["reserve"]["last"];
-  let email = document.forms["reserve"]["email"];
-  let birthdate = document.forms["reserve"]["birthdate"];
-  let quantity = document.forms["reserve"]["quantity"];
-  let location = document.forms["reserve"]["location"];
-  
-
+  let first = document.getElementById('first');
+  let last = document.getElementById('last');
   if (isName(first.value)
       && isName(last.value)
       && isNumber(quantity.value)
       && validEmail(email.value)
-      ){
-    console.log("valider");
-    // return true;
-  } else { 
-    console.log("non valider");
-    // return false;
+      && isLocation()
+      && cgv()
+      ) {
+    return true;
+  } else {
+    return false;
   }
-  return false;
+
 }
 
 
 function isName (name){
+  let nameRegex = /^[a-zA-Z-\s]+$/;
   if (name.length <= 1){
     console.log("trop court");
     return false;
   } else{
     console.log("c'est correct");
-    return true;
+    if (nameRegex.test(name) == true) {
+      console.log("c'est un nom");      
+      return true;
+    } else {
+      console.log("c'est pas un nom");
+      return false;
+    }
   }
 }
 
 function validEmail(mail) {
-  console.log("mail pas vérifier")
+  let mailRegex = /^[a-z0-9-\.]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+  if (mail=='') {
+    console.log("mail vide")
   return false;
+  } else {
+    if (mailRegex.test(mail) == true) {
+      console.log("mail valide")
+      return true;
+    } else {
+      console.log("mail non valide")
+      return false;
+    }
+  }
+  
 }
 
 function isNumber(number) {
-  if (number.length!=0) {
-    console.log("pas vide")
-    //const regex = new RegExp('^\+?\d+$');
-  } else {
-    console.log("vide correct")
+  let numberRegex = /^[0-9]+$/;
+  // TODO: faire cas si c'est vide
+  if (numberRegex.test(number) == true) {
+    console.log("c'est un nombre");
+    return true;
+  } else{
+    console.log("c'est pas un nombre");
+    return false;
+  }
+}
+
+function isLocation () {
+  /**
+   * tester si au moins un check
+   */
+  let i = 1;
+  while (document.getElementById('location'+i)) {
+    if (document.getElementById('location'+i).checked) {
+      console.log(document.getElementById('location'+i).value);
+      return true;
+    }
+    i++;
+  }
+  console.log('Pas de ville checked');
+  return false;
+}
+
+function cgv () {
+  if (cgvCheck() == 'coché') {
     return true;
   }
-  //const regex = new RegExp('^\+?\d+$');
-  //console.log(regex.test(number));
-  /*if (regex.test(number)) {
-    return true;
-  } else {
-    return false;
-  }*/
+  console.log('cgv pas checked');
   return false;
 }
