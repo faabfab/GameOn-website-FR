9cbd591da8a4013690cbce7f5770c633ea0dabd3
faabfab <faab.fab@free.fr>
2023-05-25 09:15:49 +0200

    before modal.js changement

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -24,12 +24,86 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 closeBtn.addEventListener("click", closehModal);
 
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// launch modal form
+// close modal form
 function closehModal() {
   modalbg.style.display = "none";
+}
+
+ /**
+  * Validation du formulaire:
+  * si les champs sont correctements remplies
+  *   Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+  *   Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+  *   L'adresse électronique est valide.
+  *   Pour le nombre de concours, une valeur numérique est saisie.
+  *   Un bouton radio est sélectionné.
+  *   La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
+  * alors valider
+  * sinon pas valider
+  */
+
+
+ function validate(){
+
+  // Récupération des variables
+  let first = document.forms["reserve"]["first"];
+  let last = document.forms["reserve"]["last"];
+  let email = document.forms["reserve"]["email"];
+  let birthdate = document.forms["reserve"]["birthdate"];
+  let quantity = document.forms["reserve"]["quantity"];
+  let location = document.forms["reserve"]["location"];
+  
+
+  if (isName(first.value)
+      && isName(last.value)
+      && isNumber(quantity.value)
+      && validEmail(email.value)
+      ){
+    console.log("valider");
+    // return true;
+  } else { 
+    console.log("non valider");
+    // return false;
+  }
+  return false;
+}
+
+
+function isName (name){
+  if (name.length <= 1){
+    console.log("trop court");
+    return false;
+  } else{
+    console.log("c'est correct");
+    return true;
+  }
+}
+
+function validEmail(mail) {
+  console.log("mail pas vérifier")
+  return false;
+}
+
+function isNumber(number) {
+  if (number.length!=0) {
+    console.log("pas vide")
+    //const regex = new RegExp('^\+?\d+$');
+  } else {
+    console.log("vide correct")
+    return true;
+  }
+  //const regex = new RegExp('^\+?\d+$');
+  //console.log(regex.test(number));
+  /*if (regex.test(number)) {
+    return true;
+  } else {
+    return false;
+  }*/
+  return false;
 }
